6f78d930582beb774e5ec5498215d37e6a6cd170
Vũ Minh Thư <devminhthu2023@gmail.com>
2022-06-24 09:48:33 +0700

    feat: creat UI Calculator

---
+++ calculator/src/util/Colors.ts
@@ -0,0 +1,12 @@
+export const myColors = {
+  light: '#F1F2F3',
+  dark: '#17171C',
+  blue: '#4B5EFC',
+  btnGray: '#4E505F',
+  btnDark: '#2E2F38',
+  gray: '#747477',
+  black: '#000000',
+  white: '#FFFFFF',
+  result: '#46D5B2',
+  orange: '#f69a06',
+};
